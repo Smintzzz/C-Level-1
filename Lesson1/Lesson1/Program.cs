d2c849d6af7914c947d1bb4bc079e9c1c876afbe
Sergey Gorshkov <hackmachine@mail.ru>
2019-10-31 19:47:04 +0300

    Lesson 1 Complete

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -4,14 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson1
+namespace Lesson1_Gorshkov_Sergey
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Здравствуйте! Это простейшая программа 'Анкета'. Давайте знакомиться. ");
+            
+            Console.WriteLine("Как я могу к Вам обращаться?");
+            String name = Console.ReadLine();
+            if (name == "")
+            {
+                name = "User";
+            }
+            Console.WriteLine("Здравствуйте, " + name + "!");
+
+            Console.WriteLine(name + ". Какая у Вас фамилия?");
+            String subName = Console.ReadLine();
+            
+            Console.WriteLine(name + ". Сколько Вам лет?");
+            String age = Console.ReadLine();
+            
+            Console.WriteLine(name + ". Какой у Вас рост?");
+            String growth = Console.ReadLine();
+           
+            Console.WriteLine(name + ". Какой у Вас вес?");
+            String weight = Console.ReadLine();
+           
+            Console.WriteLine(name + ". Спасибо за Ваше участие! Вот что у нас получилось: \n" + subName + " " + name + " в возрасте " + age + " лет вымахал до " + growth + " см, а весит " + weight + " кг. ");
+            String genre = Console.ReadLine();
+
+
+
+
+
             double x;
             double y;
+
+            Console.WriteLine("{0:c1}", 12345);
             Console.Write("Введите первое число: ");
             string str = Console.ReadLine();
             x = Convert.ToDouble(str);
@@ -19,6 +50,8 @@ namespace Lesson1
             y = Convert.ToDouble(Console.ReadLine());
             double z = x + y;
             Console.WriteLine(x + " + " + y + " = " + z);     // Преобразование в строку
+            Console.WriteLine("{0:c3}", z + x + "=");
+            Console.WriteLine("{0:C1}", z);
             Console.ReadLine();
         }
     }
---
+++ Lesson1/format/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace format
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person { Name = "Tom", Age = 23 };
+
+            Console.WriteLine($"Имя: {person.Name}  Возраст: {person.Age}");
+            Console.ReadLine();
+        }
+
+        class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+    
+    }
+}
